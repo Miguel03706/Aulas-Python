a87f57eae3bb650ad7c158ccab4fd6e679de1ff5
Miguel03706 <migueltjsilva@gmail.com>
2023-06-10 10:45:59 -0300

    aula 006

---
+++ Aulas/aula006.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
+
+# int // str // bool // float // type
+# métodos de verificação de tipo, ex: isNumeric() etc...
